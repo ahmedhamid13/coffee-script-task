5a23a774b4dd4a460bc869a17998c282b16a9333
Ahmed Abdelhamid <eng.a.abdelhamid@outlook.com>
2021-06-08 03:37:44 +0200

    listen to change

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,29 @@
 let gulp = require('gulp'),
     gutil = require('gulp-util'),
     uglify = require('gulp-uglify'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    livereload = require('gulp-livereload'),
+    lr = require('tiny-lr'),
+    server = lr();
 
+const jsSources = ['./components/scripts/scriptOne.js', './components/scripts/scriptTwo.js'];
 
 gulp.task('js', function () {
-    return gulp.src(['./components/scripts/scriptOne.js', './components/scripts/scriptTwo.js'])
+    return gulp.src(jsSources)
             .pipe(uglify())
             .pipe(concat('script.js'))
             .pipe(gulp.dest('./js'))
 });
+
+gulp.task('watch', function(){
+    livereload.listen();
+    gulp.watch(jsSources, gulp.series('js'));
+    gulp.watch(['./js/script.js', '*.html'], gulp.series('reload'));
+});
+
+gulp.task('reload', function() {
+    return gulp.src('./*.html')
+            .pipe(livereload());
+});
+
+gulp.task('default', gulp.series('js', 'watch'))
