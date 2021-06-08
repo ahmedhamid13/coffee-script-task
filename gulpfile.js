f79a5b37586dec4c8ad836c03f044ecbd183ae00
Ahmed Abdelhamid <eng.a.abdelhamid@outlook.com>
2021-06-08 04:11:33 +0200

    processing sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,21 @@
 let gulp = require('gulp'),
     gutil = require('gulp-util'),
     uglify = require('gulp-uglify'),
+    sass = require('gulp-ruby-sass'),
     concat = require('gulp-concat'),
     livereload = require('gulp-livereload'),
     lr = require('tiny-lr'),
     server = lr();
+
+
+const sassSources = ['./components/sass/*.scss'];
+
+gulp.task('sass', function () {
+    return sass(sassSources, {style: 'expanded', lineNumbers: true})
+            .pipe(concat('style.css'))
+            .pipe(gulp.dest('./css'))
+            .pipe(livereload())
+});
 
 const jsSources = ['./components/scripts/scriptOne.js', './components/scripts/scriptTwo.js'];
 
@@ -18,6 +29,7 @@ gulp.task('js', function () {
 gulp.task('watch', function(){
     livereload.listen();
     gulp.watch(jsSources, gulp.series('js'));
+    gulp.watch(sassSources, gulp.series('sass'));
     gulp.watch(['./js/script.js', '*.html'], gulp.series('reload'));
 });
 
@@ -26,4 +38,4 @@ gulp.task('reload', function() {
             .pipe(livereload());
 });
 
-gulp.task('default', gulp.series('js', 'watch'))
+gulp.task('default', gulp.series('sass', 'js', 'watch'))
