452af2a0768e96ec44d69df9a7e259c3da084185
Ahmed Abdelhamid <eng.a.abdelhamid@outlook.com>
2021-06-08 06:09:57 +0200

    coffee vaiables

--- components/coffee/script.coffee
+++ components/coffee/script.coffee
@@ -1,2 +1,31 @@
+arr10 = [1..10]
+arr9 = [1...10]
+console.log arr10
+console.log arr9
+
+arr_names = [
+    "Ahmed"
+    "Abd"
+    "Elhamid"
+]
+user = 
+    name: "Ahmed"
+    age: 30
+    hobbies: [
+        "Football"
+        "Chess"
+    ]
+    links: 
+        fb: "fb"
+        tw: "tw"
+
+console.log arr_names
+console.log user
+
+str = "<i>#{user.name}</i>"
+
 $ ->
-  $('h2').append 'Hello World from coffee'
+  $('h2').append "Hello World from coffee --#{str}"
+
+do ->
+    $('#content').append 'Don\'t miss it!'
--- components/scripts/coffee.js
+++ components/scripts/coffee.js
@@ -1,3 +1,35 @@
+var arr10, arr9, arr_names, str, user;
+
+arr10 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+arr9 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(arr10);
+
+console.log(arr9);
+
+arr_names = ["Ahmed", "Abd", "Elhamid"];
+
+user = {
+  name: "Ahmed",
+  age: 30,
+  hobbies: ["Football", "Chess"],
+  links: {
+    fb: "fb",
+    tw: "tw"
+  }
+};
+
+console.log(arr_names);
+
+console.log(user);
+
+str = `<i>${user.name}</i>`;
+
 $(function() {
-  return $('h2').append('Hello World from coffee');
+  return $('h2').append(`Hello World from coffee --${str}`);
 });
+
+(function() {
+  return $('#content').append('Don\'t miss it!');
+})();
